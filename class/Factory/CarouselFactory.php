e8f2ee2f72e665944f0f2eb1385d9514ca1e3471
Matt McNaney <mcnaneym@appstate.edu>
2019-04-29 13:34:13 -0400

    New options added to listing function.
    + Pin and Unpin functions added.
    + Added function to get carousel pinned to key.
    + Added transition setting saving.

--- class/Factory/CarouselFactory.php
+++ class/Factory/CarouselFactory.php
@@ -31,6 +31,7 @@ class CarouselFactory extends BaseFactory
         $carousel = $this->build();
         $carousel->title = $request->pullPostString('title');
         $carousel->iterations = $request->pullPostInteger('iterations');
+        $carousel->transition = $request->pullPostInteger('transition');
         $carousel->intervalTime = $request->pullPostInteger('intervalTime');
         $carousel->indicator = $request->pullPostInteger('indicator');
         $carousel->controls = $request->pullPostBoolean('controls');
@@ -42,6 +43,14 @@ class CarouselFactory extends BaseFactory
     {
         $db = Database::getDB();
         $tbl = $db->addTable('caro_carousel');
+        if (!empty($options['titleOnly'])) {
+            $tbl->addField('id');
+            $tbl->addField('title');
+        }
+
+        if (!empty($options['notPinned'])) {
+            $tbl->addFieldConditional('id', $options['notPinned'], '!=');
+        }
         $tbl->addOrderBy('title');
         $result = $db->select();
         return $result;
@@ -52,6 +61,7 @@ class CarouselFactory extends BaseFactory
         $carousel = $this->load($id);
         $carousel->title = $request->pullPutString('title');
         $carousel->iterations = $request->pullPutInteger('iterations');
+        $carousel->transition = $request->pullPutInteger('transition');
         $carousel->intervalTime = $request->pullPutInteger('intervalTime');
         $carousel->indicator = $request->pullPutInteger('indicator');
         $carousel->controls = $request->pullPutBoolean('controls');
@@ -65,8 +75,9 @@ class CarouselFactory extends BaseFactory
         $slideFactory->deleteByCarouselId($carousel->id);
         self::deleteResource($carousel);
     }
-    
-    public function getHomeCarousel() {
+
+    public function getHomeCarousel()
+    {
         $db = Database::getDB();
         $tbl = $db->addTable('caro_carousel');
         $tbl->addFieldConditional('frontpage', 1);
@@ -77,7 +88,7 @@ class CarouselFactory extends BaseFactory
         }
         return $carousel[0];
     }
-    
+
     /**
      * Toggles the selected carousel to front page status
      * @param int $carouselId
@@ -94,14 +105,49 @@ class CarouselFactory extends BaseFactory
             self::saveResource($carousel);
         }
     }
-    
+
     /**
      * Removes front page status from ALL carousels.
      */
-    private function removeFrontPage() {
+    private function removeFrontPage()
+    {
         $db = Database::getDB();
         $tbl = $db->addTable('caro_carousel');
         $tbl->addValue('frontpage', 0);
         $db->update();
     }
+
+    public function unpin(int $carouselId, int $keyId)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('caro_pin');
+        $tbl->addFieldConditional('carouselId', $carouselId);
+        $tbl->addFieldConditional('keyId', $keyId);
+        $db->delete();
+    }
+
+    public function pin(int $carouselId, int $keyId)
+    {
+        $this->unpin($carouselId, $keyId);
+        $db = Database::getDB();
+        $tbl = $db->addTable('caro_pin');
+        $tbl->addValue('carouselId', $carouselId);
+        $tbl->addValue('keyId', $keyId);
+        $db->insert();
+    }
+
+    public function getPinned($keyId)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('caro_pin');
+        $tbl->addField('carouselId');
+        $tbl->addFieldConditional('keyId', $keyId);
+        $result = $db->selectColumn();
+        if (empty($result)) {
+            return;
+        }
+        $carousel = $this->load($result);
+        return $carousel;
+    }
+
 }
