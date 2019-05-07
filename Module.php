67ed43dc12db5bc4208a45213f750f614402d08c
Matt McNaney <mcnaneym@appstate.edu>
2019-05-07 09:33:33 -0400

    Added link to current slide admin from the view.
    + Added error checks to prevent errors prior to update.
    + Removed deprecated code.

--- Module.php
+++ Module.php
@@ -13,6 +13,7 @@ use Canopy\Response;
 use Canopy\Server;
 use Canopy\SettingDefaults;
 use carousel\View\CarouselView;
+use carousel\Factory\CarouselFactory;
 use carousel\Controller\Controller;
 
 if (is_file(PHPWS_SOURCE_DIR . 'mod/carousel/config/defines.php')) {
@@ -73,14 +74,29 @@ class Module extends \Canopy\Module
 
     public function runTime(Request $request)
     {
+        if (version_compare($this->version, '2.0.0', '<')) {
+            return;
+        }
         if (!$request->isVar('module')) {
             $view = new CarouselView;
-            \Layout::add($view->homeView(), 'carousel', 'slides');
+            $factory = new CarouselFactory;
+            $carousel = $factory->getHomeCarousel();
+            $homeView = $view->view($carousel);
+            if (!empty($homeView)) {
+                \Layout::add($homeView, 'carousel', 'slides');
+                if (\Current_User::allow('carousel')) {
+                    \MiniAdmin::add('carousel',
+                            '<a href="./carousel/Admin/Slide/?carouselId=' . $carousel->id . '">Edit current slides</a>');
+                }
+            }
         }
     }
 
     public function afterRun(Request $request, \Canopy\Response $response)
     {
+        if (version_compare($this->version, '2.0.0', '<')) {
+            return;
+        }
         $key = \Canopy\Key::getCurrent();
         if ($key && !$key->isDummy() && !$key->isHomeKey()) {
             if (\Current_User::allow('carousel')) {
@@ -95,7 +111,7 @@ class Module extends \Canopy\Module
         $carouselView = new View\CarouselView;
         \Layout::add($carouselView->viewPinned($keyId), 'carousel', 'slides');
     }
-    
+
     private function pinForm($keyId)
     {
         $carouselView = new View\CarouselView;
@@ -103,71 +119,6 @@ class Module extends \Canopy\Module
         \MiniAdmin::add('carousel', $content);
     }
 
-    /*
-      public function runTime(\Canopy\Request $request)
-      {
-      if (!$request->isVar('module')) {
-      $display = \carousel\SlideFactory::display();
-      if (!empty($display)) {
-      \Layout::add($display, 'carousel', 'slides');
-      }
-      }
-      }
-
-      public function afterRun(\Canopy\Request $request,
-      \Canopy\Response $response)
-      {
-      $key = \Canopy\Key::getCurrent();
-      if ($key && !$key->isDummy()) {
-      $this->checkKey($key->id);
-      }
-      }
-
-      private function checkKey($key_id)
-      {
-      \Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP
-      . 'mod/carousel/javascript/add_slide.js"></script>');
-      $db = \phpws2\Database::newDB();
-      $t = $db->addTable('caro_keyed_slide');
-      $t->addField('slide_id');
-      $t->addFieldConditional('key_id', $key_id);
-      $result = $db->selectColumn();
-      if (\Current_User::allow('carousel')) {
-      $this->miniAdmin($result, $key_id);
-      }
-      if (!empty($result)) {
-      \carousel\SlideFactory::showKeySlide($result);
-      }
-      }
-
-      private function miniAdmin($result, $key_id)
-      {
-      if (empty($result)) {
-      $db = \phpws2\Database::newDB();
-      $t2 = $db->addTable('caro_slide');
-      $t2->addOrderBy('title');
-      $t2->addField('title');
-      $t2->addField('id');
-      $slides = $db->select();
-      if (empty($slides)) {
-      return;
-      }
-
-      $opt[] = '<option id="0" style="">' . 'Add slide to this page' . '</option>';
-      foreach ($slides as $s) {
-      $opt[] = '<option value="' . $s['id'] . '">' . substr($s['title'],
-      0, 15) . '</option>';
-      }
-
-      $select = '<select data-key-id="' . $key_id . '" id="add-slide" style="font-size:12px" class="form-control">'
-      . implode("\n", $opt) . '</select>';
-      } else {
-      $select = '<a href="javascript:void(0)" id="remove-slide" data-key-id="' . $key_id . '">' . 'Remove slide from page' . '</a>';
-      }
-      \MiniAdmin::add('carousel', $select);
-      }
-     *
-     */
 }
 
 ?>
