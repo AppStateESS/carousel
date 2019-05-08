769f7fa82478770332267afcb67aca762138c823
Matt McNaney <mcnaneym@appstate.edu>
2019-05-08 10:46:19 -0400

    Raised version

--- boost/update.php
+++ boost/update.php
@@ -1,8 +1,8 @@
 <?php
+
 /**
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  */
-
 require_once PHPWS_SOURCE_DIR . 'mod/carousel/boost/Version2.php';
 
 function carousel_update(&$content, $version)
@@ -25,13 +25,21 @@ function carousel_update(&$content, $version)
         case version_compare($version, '2.0.0', '<'):
             $version2 = new Version2;
             $version2->runUpdate();
-            
+
             $content[] = '<pre>2.0.0
 --------------------
 + Rewrite of Carousel
 + Multiple Carousels
 + Ability to use videos
 + Pinning of Carousels to pages.
+</pre>';
+
+        case version_compare($version, '2.0.1', '<'):
+            $content[] = '<pre>2.0.1
+--------------------
++ Fixed relative urls.
++ Fixed url links on captions.
++ Fixed text orientation on captioning.
 </pre>';
     } // end of switch
 
