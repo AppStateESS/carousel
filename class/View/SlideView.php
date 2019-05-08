0551571c398a115b9a0d5cde08e0a65ded48a119
Matt McNaney <mcnaneym@appstate.edu>
2019-05-08 10:03:23 -0400

    Url is massaged before displaying.

--- class/View/SlideView.php
+++ class/View/SlideView.php
@@ -30,7 +30,6 @@ class SlideView extends AbstractView
     private function getSlideVars(SlideResource $slide, bool $active = false)
     {
         $vars = $slide->getStringVars();
-        $vars['url'] = $slide->removeUrlProtocol();
         $vars['active'] = $active;
 
         switch ($slide->caption_zone) {
@@ -63,18 +62,37 @@ class SlideView extends AbstractView
                 $vars['vertical'] = 'bottom';
                 break;
         }
-        
+
         if (!empty($slide->url) && !empty($slide->title)) {
-            $vars['caption_title'] = '<a href="//' . $slide->removeUrlProtocol() . '">' . $slide->title . '</a>';
+            $vars['caption_title'] = '<a href="' . $this->prepareUrl($slide->url) . '">' . $slide->title . '</a>';
         } else {
             $vars['caption_title'] = $slide->title;
         }
         return $vars;
     }
 
+    private function prepareUrl(string $url)
+    {
+        if (preg_match('@^\./@', $url)) {
+            $url = \Canopy\Server::getSiteUrl() . str_replace('./', '', $url);
+        } else {
+            $url = '//' . $this->removeUrlProtocol($url);
+        }
+        return $url;
+    }
+
+    /**
+     * @returns string
+     */
+    private function removeUrlProtocol($url)
+    {
+        return preg_replace('@^https?:/\/@', '', $url);
+    }
+
     public function imageSlide(SlideResource $slide, bool $active = false)
     {
         $vars = $this->getSlideVars($slide, $active);
+        $vars['url'] = $this->prepareUrl($vars['url']);
         $template = new Template($vars);
         $template->setModuleTemplate('carousel', 'imageSlide.html');
         return $template->get();
@@ -83,6 +101,7 @@ class SlideView extends AbstractView
     public function mediaSlide(SlideResource $slide, bool $active = false)
     {
         $vars = $this->getSlideVars($slide, $active);
+        $vars['url'] = $this->prepareUrl($vars['url']);
         $vars['videoType'] = preg_match('/\.mp4$/', $slide->filepath) ? 'video/mp4' : 'video/webm';
         $template = new Template($vars);
         $template->setModuleTemplate('carousel', 'mediaSlide.html');
